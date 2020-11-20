9bc1a84d59020c40d8f54c15cb9797772b0e9de5
SeanXiaoanZhang <SeanZhangXiaoan@gmail.com>
2020-11-20 01:11:24 -0500

    project init structure

---
+++ mall-coupon/src/test/java/com/shawn/mall/order/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shawn.mall.order.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mall-ware/src/main/java/com/shawn/mall/order/ware/MallWareApplication.java
@@ -0,0 +1,13 @@
+package com.shawn.mall.order.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallWareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallWareApplication.class, args);
+	}
+
+}
